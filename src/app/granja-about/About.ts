112737dd0f2cf32f8c5e1d467f864d0cf90aeee0
Kevin Garces Hoyberg <kevin.garceshoyberg@gmail.com>
2021-06-25 16:31:13 -0300

    modificado componente  about

---
+++ src/app/granja-about/About.ts
@@ -0,0 +1,9 @@
+export interface About {
+
+    name: string,
+    adress: string,
+    telefono: number,
+    email: string,
+    instagram: string;
+
+}
--- src/app/granja-about/granja-about.component.ts
+++ src/app/granja-about/granja-about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { About } from './About';
 
 @Component({
   selector: 'app-granja-about',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./granja-about.component.scss']
 })
 export class GranjaAboutComponent implements OnInit {
+
+
+  about: About = 
+    {
+    name: 'Granja Avicola',
+    adress: 'Necochea - Ruta 86, KM 3',
+    telefono: 2262590909,
+    email: 'GranjaAvicola@gmail.com',
+    instagram: 'GranjaAvicola.ok',
+    };
+  
+
 
   constructor() { }
 
